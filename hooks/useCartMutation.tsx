c0f34abf511aa225a5f621a39efda1ea203406eb
fatihcaliss <fthcls@gmail.com>
2024-02-26 04:28:58 +0300

    refactor middleware.ts

--- hooks/useCartMutation.tsx
+++ hooks/useCartMutation.tsx
@@ -1,11 +1,15 @@
 import { patchUserCart } from "@/services";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 
 const useCartMutation = () =>
   useMutation({
     mutationFn: patchUserCart,
     onSuccess: (data) => {
-      console.log(data);
+      toast.success(
+        "Congratulations! 🎉 You have successfully added a product to your cart!"
+      );
+      // console.log(data);
     },
     onError: (error) => {
       console.log(error);
