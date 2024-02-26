8759feac496f82c185a2b97304ded418e2bd00f5
fatihcaliss <fthcls@gmail.com>
2024-02-26 05:20:52 +0300

    test authOpritons

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,54 @@
 import NextAuth from "next-auth/next";
-import { authOptions } from "@/app/lib/auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        try {
+          const res = await fetch("https://dummyjson.com/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+          });
+
+          const userData = await res.json();
+
+          const response = await fetch("https://dummyjson.com/auth/me", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${userData?.token}`,
+            },
+          });
+          const user = await response.json();
+
+          if (user.id) {
+            return user;
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
