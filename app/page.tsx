488cdf1e40b640f6ba769503b7f33b392c7563a5
fatihcaliss <fthcls@gmail.com>
2024-02-26 05:29:49 +0300

    fix vercel teployment causer authOptions

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import LoginForm from "@/components/LoginForm/LoginForm";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
